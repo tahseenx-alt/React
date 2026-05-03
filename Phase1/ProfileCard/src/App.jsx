4ee786020a52cfb0998990faa232591439752bf4
Tahseen Anwer <tahseenanwer100@gmail.com>
2026-05-03 18:40:53 +0530

    updating the ui

--- Phase1/ProfileCard/src/App.jsx
+++ Phase1/ProfileCard/src/App.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 function App() {
   return (
-    <div className='text-xl'>
+    <div className=' flex flex-col  items-center m-30 gap-10 text-xl'>
       Profile Card 
+      <div className='flex items-center h-150 w-120 bg-cyan-900 rounded-4xl'>
+        <div className='h-2 w-120 bg-amber-800'></div>
+      </div>
     </div>
   )
 }
