81bb4809916550cdbb60d62fde0d7e7223a69a0f
Tahseen Anwer <tahseenanwer100@gmail.com>
2026-05-04 19:56:21 +0530

    Adding Button

--- Phase1/ProfileCard/src/App.jsx
+++ Phase1/ProfileCard/src/App.jsx
@@ -3,9 +3,12 @@ import React from 'react'
 function App() {
   return (
     <div className=' flex flex-col  items-center m-30 gap-10 text-xl'>
-      Profile Card 
+      Profile Card
       <div className='flex items-center h-150 w-120 bg-cyan-900 rounded-4xl'>
         <div className='h-2 w-120 bg-amber-800'></div>
+      </div>
+      <div>
+        <button className='h-20 w-60 bg-cyan-950 rounded-full '>Add</button>
       </div>
     </div>
   )
