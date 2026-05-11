8abe0465c252c9e8dc636d963b336394f34fa34a
Tahseen Anwer <tahseenanwer100@gmail.com>
2026-05-11 20:47:13 +0530

    Adding profile

--- Phase1/ProfileCard/src/App.jsx
+++ Phase1/ProfileCard/src/App.jsx
@@ -4,8 +4,15 @@ function App() {
   return (
     <div className=' flex flex-col  items-center m-30 gap-10 text-xl'>
       Profile Card
-      <div className='flex items-center h-150 w-120 bg-cyan-900 rounded-4xl'>
-        <div className='h-2 w-120 bg-amber-800'></div>
+      <div className='flex flex-col items-center h-150 w-120 gap-5 bg-cyan-900 rounded-4xl'>
+        <div className='flex h-1/4 w-full  rounded-full bg-amber-300'>
+          <img className='h-3/4 m-5 rounded-full object-cover'
+          src="https://images.unsplash.com/photo-1544723795-3fb6469f5b39?q=80&w=2592&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+          <div className='p-5 text-black' > Tahseen Anwer</div>
+        </div>
+        <div className='h-3/4 w-full p-5 rounded-4xl bg-amber-300'>
+            Details
+        </div>
       </div>
       <div className=' flex gap-5'>
         <div>
